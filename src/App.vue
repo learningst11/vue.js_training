b5b3918de9b29b9f3d0a9b373e893daebcc44e5d
learningst11 <learningst11@gmail.com>
2022-09-22 02:21:43 +0900

    Add blog list vue component
    Signed-off-by: learningst11 <learningst11@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Vuelog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-4">
+  <h5>React 개발자의 블로그입니다</h5>
+  <p>- Vue로 만들었음 -</p>
+</div>
+
+<List: 블로그글="블로그글"  />
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue';
+import blog from './assets/blog.js';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      블로그글 : blog
+    }
+  },
   components: {
-    HelloWorld
+    List:List,
   }
 }
 </script>
@@ -21,6 +58,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h5>{{블로그글[0].title}}</h5>
+    <p>{{블로그글[0].date}}</p>
+  </div>
+
+  <div>
+    <h5></h5>
+    <p></p>
+  </div>
+
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜</p>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+    name:'list',
+    props:{
+      블로그글 : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
